63e7a66490e966b3441ea171a6ed865bd34b890f
satya sindhu <movva001@gmail.com>
2021-11-08 08:14:16 -0500

    modified index2.js

---
+++ index2.js
@@ -0,0 +1,24 @@
+/*const {fetchMyIP,fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss_promised');
+fetchMyIP()
+  .then(body => fetchCoordsByIP(body))
+  .then(body => fetchISSFlyOverTimes(body))
+  .then((body) => printPassTimes(JSON.parse(body)));
+*/
+const {nextISSTimesForMyLocation} = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+  
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
